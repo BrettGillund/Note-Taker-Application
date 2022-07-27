57b9c14414968fa933ec61b0bf49bbc5bc416744
Brett Gillund <brettgillund2@gmail.com>
2022-07-27 17:55:30 -0500

    changed front end and back end notes to add api prefex. Fixed api call on server route.

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -4,7 +4,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/api/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -25,13 +25,23 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () =>
-  fetch('/api/notes', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+const getNotes = () =>{
+  return fetch('/api/notes', {
+  }).then((res) => (
+      console.log(res.json)
+    )).then((data) => (
+      console.log({
+        line34: data
+      })
+      )).catch((err) => (
+        console.error(err)
+        )).finally(() => console.info('reached finally block'));
+      };
+      
+      // method: 'GET',
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // },
 
 const saveNote = (note) =>
   fetch('/api/notes', {
@@ -119,7 +129,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/api/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -165,15 +175,15 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/api/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => getNotes().then((notes) => renderNoteList(notes)).catch((err) => console.log(err));
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/api/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,23 +1,34 @@
 const note_router = require('express').Router();
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const db_path = path.join(__dirname, '../db/db.json');
-const note_path = path.join(__dirname, './public/notes.html')
+const note_path = path.join(__dirname, './public/notes.html');
 
 
 // write functions to get front end working properly.
 
 function getNoteData() {
-    return fs.promises
+    return fs
         .readFile(db_path, 'utf8')
         .then(data =>JSON.parse(data));
 };
 
 // route user to the /notes section which is the notes.html
 note_router.get('/notes', (request, response) =>{
-    request
-    response
+
+    getNoteData().then((response) => (
+        response.status(200).json({
+            message: 'Notes received'
+        }).catch((err) => (
+            console.log(err)
+        ))
+    ))
+    // console.log(request);
+    // console.log(response);
+    // console.log(getNoteData());
 });
+
+
 
 
 
